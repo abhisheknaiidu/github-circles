347e9ddfd311c1ae34db84980f5f5233434edfc9
Raj Varsani <varasaniraj5@gmail.com>
2024-02-10 17:09:49 +0530

    feat: create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
-import { cookies } from "next/headers";
+import GitHubIcon from "@/assets/GitHubIcon.svg";
+import Image from "next/image";
 import Link from "next/link";
 
-import { getUser, userCookieKey } from "../libs/session";
-
 export default function Home() {
-  const cookieStore = cookies();
-  const userCookie = cookieStore.get(userCookieKey);
-  const user = getUser(userCookie?.value ?? "");
-  console.log("user", user, userCookie);
-  if (user) {
-    return <a href={``}>user: {user}</a>;
-  }
+  // const cookieStore = cookies();
+  // const userCookie = cookieStore.get(userCookieKey);
+  // const user = getUser(userCookie?.value ?? "");
+  // console.log("user", user, userCookie);
+  // if (user) {
+  //   return <a href={``}>user: {user}</a>;
+  // }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {<div className="text-3xl font-bold underline">github circles</div>}
-      <Link href="/auth">
-        <button className={""}>Login to Add</button>
+    <main className="flex flex-col items-center justify-center h-full gap-6 p-4 pb-12 min-8">
+      {
+        <div className="text-[4rem] leading-[5.2rem] font-semibold tracking-tighter text-center md:leading-[6.8rem] md:text-[5.75rem]">
+          Generate
+          <br />
+          GitHub Circle
+        </div>
+      }
+      <Link
+        href="/auth"
+        className="flex items-center justify-center gap-4 p-3 px-6 text-lg transition-all bg-black rounded-full backdrop-blur-sm bg-opacity-20 w-fit hover:bg-opacity-30 text-slate-200"
+      >
+        <Image src={GitHubIcon} alt="GitHub" width={20} height={20} />
+        {"Login with Github"}
       </Link>
+      <p className="text-center text-slate-300 max-w-[34rem] font-light">
+        Transform your GitHub connections into captivating circle image. Craft a
+        unique representation of your coding journey and standout collaborators
+        in your projects.
+      </p>
     </main>
   );
 }
