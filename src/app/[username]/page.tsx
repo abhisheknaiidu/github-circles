6f0d7332bffd637bd339dc38de5b76925d102d88
Raj Varsani <varasaniraj5@gmail.com>
2024-02-11 18:28:22 +0530

    feat: add circle rendering

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -8,30 +8,76 @@ import XIcon from "@/assets/XIcon.svg";
 import { useElementSize } from "@/hooks/useElementsSize";
 import Image from "next/image";
 import { useParams } from "next/navigation";
-import { CSSProperties, useEffect, useRef } from "react";
+import { CSSProperties, useEffect, useMemo, useRef, useState } from "react";
 import domtoimage from "dom-to-image";
+
+const layerProperties = [
+  {
+    count: 1,
+    avatarScale: 12,
+    radius: 0,
+  },
+  {
+    count: 8,
+    avatarScale: 9,
+    radius: 1.6,
+  },
+  {
+    count: 12,
+    avatarScale: 7,
+    radius: 2.9,
+  },
+  {
+    count: 20,
+    avatarScale: 5.5,
+    radius: 4,
+  },
+];
+
+const cumulativeLayersIndex = layerProperties.reduce<number[]>((acc, layer) => {
+  if (acc.length > 0) {
+    acc.push(acc[acc.length - 1] + layer.count);
+  } else {
+    acc = [layer.count];
+  }
+  return acc;
+}, []);
+console.log(cumulativeLayersIndex);
 
 const UserImageCard = (props: {
   user: { name: string; login: string; avatar_url: string };
   scale: number;
 }) => {
+  const [loaded, setLoaded] = useState(false);
   return (
-    <div
-      className="absolute p-2 rounded-full"
-      style={{
-        height: `calc(var(--size) * ${props.scale} / 100)`,
-        width: `calc(var(--size) * ${props.scale} / 100)`,
-        background: "#EDE5FF",
-      }}
-    >
-      <div className="absolute w-full h-full overflow-hidden">
+    <div className="relative flex items-center justify-center">
+      <div
+        className="absolute rounded-full mix-blend-overlay"
+        style={{
+          height: `calc((var(--size) / 100 + .06rem) * ${props.scale} )`,
+          width: `calc((var(--size) / 100 + .06rem) * ${props.scale} )`,
+          background: "#EDE5FF",
+        }}
+      ></div>
+      <div
+        className="absolute overflow-hidden bg-white rounded-full"
+        style={{
+          height: `calc(var(--size) * ${props.scale} / 100 )`,
+          width: `calc(var(--size) * ${props.scale} / 100 )`,
+          background: "#EDE5FF",
+        }}
+      >
         <Image
           src={props.user.avatar_url}
           alt={props.user.name}
-          // width={props.size}
-          // height={props.size}
           layout="fill"
-          className="rounded-full"
+          className="transition-opacity"
+          style={{
+            opacity: loaded ? 1 : 0,
+          }}
+          onLoad={() => {
+            setLoaded(true);
+          }}
         />
       </div>
     </div>
@@ -43,105 +89,16 @@ function Page() {
   const { ref: containerRef, width, height } = useElementSize<HTMLDivElement>();
   const circleRef = useRef<HTMLButtonElement>(null);
 
-  const circleData = {
-    user: {
-      name: "Frank Martin",
-      login: "frankm",
-      avatar_url: "https://avatars.githubusercontent.com/u/445567?v=4",
-    },
-    connections: [
-      {
-        name: "The Octocat",
-        login: "octocat",
-        avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
-      },
-      {
-        name: "John Doe",
-        login: "johndoe",
-        avatar_url: "https://avatars.githubusercontent.com/u/123456?v=4",
-      },
-      {
-        name: "Jane Smith",
-        login: "janesmith",
-        avatar_url: "https://avatars.githubusercontent.com/u/789012?v=4",
-      },
-      {
-        name: "Alice Johnson",
-        login: "alicej",
-        avatar_url: "https://avatars.githubusercontent.com/u/345678?v=4",
-      },
-      {
-        name: "Bob Williams",
-        login: "bobw",
-        avatar_url: "https://avatars.githubusercontent.com/u/901234?v=4",
-      },
-      {
-        name: "Charlie Brown",
-        login: "charlieb",
-        avatar_url: "https://avatars.githubusercontent.com/u/567890?v=4",
-      },
-      {
-        name: "Eva Davis",
-        login: "evad",
-        avatar_url: "https://avatars.githubusercontent.com/u/112233?v=4",
-      },
-      {
-        name: "Frank Martin",
-        login: "frankm",
-        avatar_url: "https://avatars.githubusercontent.com/u/445566?v=4",
-      },
-      {
-        name: "Grace Taylor",
-        login: "gracet",
-        avatar_url: "https://avatars.githubusercontent.com/u/778899?v=4",
-      },
-      {
-        name: "Henry Wilson",
-        login: "henryw",
-        avatar_url: "https://avatars.githubusercontent.com/u/990011?v=4",
-      },
-      {
-        name: "Ivy White",
-        login: "ivyw",
-        avatar_url: "https://avatars.githubusercontent.com/u/223344?v=4",
-      },
-      {
-        name: "Jack Harris",
-        login: "jackh",
-        avatar_url: "https://avatars.githubusercontent.com/u/556677?v=4",
-      },
-      {
-        name: "Karen Miller",
-        login: "karenm",
-        avatar_url: "https://avatars.githubusercontent.com/u/112233?v=4",
-      },
-      {
-        name: "Leo Thompson",
-        login: "leot",
-        avatar_url: "https://avatars.githubusercontent.com/u/334455?v=4",
-      },
-      {
-        name: "Mia Garcia",
-        login: "miag",
-        avatar_url: "https://avatars.githubusercontent.com/u/667788?v=4",
-      },
-      {
-        name: "Nathan Turner",
-        login: "natet",
-        avatar_url: "https://avatars.githubusercontent.com/u/112233?v=4",
-      },
-      {
-        name: "Olivia Clark",
-        login: "oliviac",
-        avatar_url: "https://avatars.githubusercontent.com/u/112233?v=4",
-      },
-      {
-        name: "Paul Reed",
-        login: "paulr",
-        avatar_url: "https://avatars.githubusercontent.com/u/112233?v=4",
-      },
-    ],
-  };
+  // use memo to avoid re-rendering
+  const circleData = useMemo(() => {
+    return new Array(50).fill(0).map((_, index) => ({
+      name: "The Octocat",
+      login: "octocat",
+      avatar_url: `https://avatars.githubusercontent.com/u/${Math.floor(
+        Math.random() * 1000000
+      )}?v=4`,
+    }));
+  }, []);
 
   const generateImage = async () => {
     if (circleRef.current) {
@@ -154,19 +111,19 @@ function Page() {
   const size = Math.min(width, height);
   return (
     <div
-      className="grid items-center w-full h-full gap-6 px-4"
+      className="grid items-center w-full h-full gap-6 px-4 pt-4"
       style={{
         gridTemplateRows: "1fr auto",
       }}
     >
       <div
-        className="flex items-center justify-center w-full h-full animate-fade"
+        className="flex items-center justify-center w-full h-full"
         ref={containerRef}
       >
-        {size &&
+        {size > 0 &&
           (circleData ? (
             <div
-              className="relative flex items-center justify-center overflow-hidden rounded-3xl h-[var(--size)] w-[var(--size)]"
+              className="animate-fade relative flex items-center justify-center overflow-hidden rounded-3xl h-[var(--size)] w-[var(--size)]"
               style={
                 {
                   "--size": size + "px",
@@ -186,31 +143,50 @@ function Page() {
                 <Image
                   src={CircleBG}
                   alt="Circle Background"
-                  className="absolute"
+                  className="absolute hidden"
                   height={(size * 3.25) / 4}
                   width={(size * 3.25) / 4}
                 />
-                <div
-                  className="absolute rounded-full mix-blend-overlay"
-                  style={{
-                    height: "calc(var(--size) * 3 / 4)",
-                    width: "calc(var(--size) * 3 / 4)",
-                    background: "#EDE5FF88",
-                    outline: "6px solid #EDE5FF",
-                    boxShadow: "0px 4px 69.8px 5px rgba(237, 229, 255, 0.20)",
-                  }}
-                />
-                <div
-                  className="absolute rounded-full mix-blend-overlay"
-                  style={{
-                    height: "calc(var(--size) * 2 / 5)",
-                    width: "calc(var(--size) * 2 / 5)",
-                    background: "gba(237, 229, 255, 0.30)",
-                    outline: "6px solid #EDE5FF",
-                    boxShadow: "0px 4px 69.8px 5px rgba(237, 229, 255, 0.20)",
-                  }}
-                />
-                <UserImageCard user={circleData.user} scale={12} />
+
+                {/* outline divs */}
+                {layerProperties.map((layer, layerIndex) => (
+                  <div
+                    key={layerIndex}
+                    className="absolute rounded-full mix-blend-overlay"
+                    style={{
+                      height: `calc(var(--size) * ${layer.radius} / 5 - 6px)`,
+                      width: `calc(var(--size) * ${layer.radius} / 5 - 6px)`,
+                      background: "#EDE5FF88",
+                      outline: "6px solid #EDE5FF",
+                      boxShadow: "0px 4px 69.8px 5px rgba(237, 229, 255, 0.20)",
+                    }}
+                  />
+                ))}
+
+                {/* user cards */}
+                {layerProperties.map((layer, layerIndex) => {
+                  return circleData
+                    .slice(
+                      cumulativeLayersIndex[layerIndex - 1],
+                      cumulativeLayersIndex[layerIndex]
+                    )
+                    .map((user, index) => (
+                      <div
+                        key={index}
+                        className="absolute"
+                        style={{
+                          transform: `rotate(${
+                            (360 / layer.count) * index
+                          }deg) translate(0, calc(var(--size) * ${
+                            layer.radius
+                          } / 10)) rotate(${(360 / layer.count) * -index}deg)`,
+                          top: "50%",
+                        }}
+                      >
+                        <UserImageCard user={user} scale={layer.avatarScale} />
+                      </div>
+                    ));
+                })}
               </div>
             </div>
           ) : (
