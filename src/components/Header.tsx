f38552d2cb74cbeb9491f1b9bfa9fd499ea4383a
Raj Varsani <varasaniraj5@gmail.com>
2024-02-12 22:44:24 +0530

    feat: update header styling
    - add support for small sizes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,14 +15,19 @@ const LINKS = [
 
 function Header() {
   return (
-    <div className="flex items-center justify-between w-full pr-1">
+    <div className="flex items-center justify-between w-full md:pr-1">
       <Link href="/">
-        <Image priority src={Logo} alt="Github Circle Logo" />
+        <Image
+          priority
+          src={Logo}
+          alt="Github Circle Logo"
+          className="h-10 md:h-12 w-fit"
+        />
       </Link>
-      <div className="flex items-center gap-8">
+      <div className="flex items-center gap-8 ">
         {LINKS.map((link) => (
           <Link key={link.href} href={link.href} target="_blank">
-            <p className="text-slate-300">{link.label}</p>
+            <p className="text-sm text-slate-300 md:text-base">{link.label}</p>
           </Link>
         ))}
       </div>
